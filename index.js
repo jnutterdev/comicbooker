acbdb783af1673925588a1ff63c9cf721453c951
terribledevice <longlost@gmail.com>
2020-11-10 22:51:57 -0500

    getting main layout going along with bulma

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let express = require('express');
 let app = express();
+let path = require('path');
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use('/css', express.static(__dirname + '/node_modules/bulma/css'));
 
-app.get('/', (req, res) => {
-    res.render('index', { foo: 'FOO' });
-});
+app.get('/', function(req, resp) {
+    resp.render('index')
+})
 
-app.listen(4000, () => console.log('Example app listening on port 4000!'));
+app.listen(3000, () => console.log(`App running on http://localhost:3000`));
