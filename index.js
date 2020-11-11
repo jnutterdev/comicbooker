a1f5593c002d0ef78f85dbc5b236f47cfdf3d5eb
terribledevice <longlost@gmail.com>
2020-11-10 23:46:10 -0500

    Got some styling going and layout

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 let express = require('express');
 let app = express();
+const port = 3000;
 let path = require('path');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use('/css', express.static(__dirname + '/node_modules/bulma/css'));
+app.use(express.static('static'));
 
 app.get('/', function(req, resp) {
     resp.render('index')
 })
 
-app.listen(3000, () => console.log(`App running on http://localhost:3000`));
+app.listen(port, () => console.log(`App running on http://localhost:${port}`));
