7a17946010f3805c21d0c57ec3b3518b5145d764
John Nutter <john@terribledevice.com>
2020-11-17 16:10:15 -0500

    got info to pull in from json file

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 let express = require('express');
 let app = express();
-const port = 3000;
+let port = 3000;
 let path = require('path');
+
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use('/css', express.static(__dirname + '/node_modules/bulma/css'));
 app.use(express.static('static'));
 
+app.locals.comicbookdata = require('./models/comicsdb.json');
+
 app.get('/', function(req, resp) {
-    resp.render('index')
+    resp.render('layout')
 })
 
 app.listen(port, () => console.log(`App running on http://localhost:${port}`));
--- models/Comics.js
+++
@@ -1,4 +0,0 @@
-module.exports = comicbooks.model('Comic', {
-    comicTitle: 'products',
-    hasTimestamps: ['createdAt', 'updatedAt', 'deletedAt']
-});
